00604e277f2cc7996fba0e44580bca4b5dab6db7
huangkc <huangkaichun@maiditech.com>
2017-08-28 16:56:59 +0800

    add setOnClickListener

---
+++ app/src/main/java/com/maidi/uicustomview/TitleLayout.java
@@ -0,0 +1,39 @@
+package com.maidi.uicustomview;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2017/8/28.
+ */
+
+public class TitleLayout extends LinearLayout {
+    public TitleLayout(Context context, AttributeSet attributeSet){
+        super(context,attributeSet);
+        LayoutInflater.from(context).inflate(R.layout.title,this);
+        Button titleBack = (Button)findViewById(R.id.title_back);
+        Button titleEdit = (Button)findViewById(R.id.title_edit);
+
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Activity) getContext()).finish();
+            }
+        });
+
+        titleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"You Clicked EditButton",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+}
